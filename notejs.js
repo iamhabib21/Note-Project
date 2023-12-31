852685609249853d1a4cc73ef199007ea9ad2da4
iamhabib21 <134780698+iamhabib21@users.noreply.github.com>
2023-12-31 22:54:47 +0100

    Add files via upload

---
+++ notejs.js
@@ -0,0 +1,22 @@
+const notesContainer = document.querySelector(".notes-container");
+const createBn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box")
+
+
+
+createBn.addEventListener("click", ()=>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable","true");
+    img.src = "images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img)
+})
+
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "IMG"){
+        e.target.parentElement.remove();
+
+    }
+
+})
